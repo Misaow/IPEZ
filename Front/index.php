9970aae67c9380ba85f3318d18352bfca64c2d69
Misaow <yoann.pierre93@gmail.com>
2014-06-02 16:58:52 +0200

    change index

--- Front/index.php
+++ Front/index.php
@@ -5,13 +5,41 @@
         <meta name="keywords" content="keywords" />
         <meta name="author" content="IPEZ" />
         <link rel="shortcut icon" href="../favicon.ico">
-        <link rel="stylesheet" type="text/css" href="content/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="content/css/custom.css" />
+        <link rel="stylesheet" type="text/css" href="app/content/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="app/content/css/custom.css" />
     </head>
     <body>
-        
- 
+        <!-- Header -->
+        <header>
+            <div class="col-sm-7"><img src="public_html/images/logo.png" alt="logo"></div>
+            <div class="col-sm-5"><p id="slogan">Vos voitures durent plus longtemps avec IP Motors</p></div>
+        </header>
+        <!--
+        <div class="navbar navbar-inverse navbar-static-top">
+        <a href="#" class="navbar-brand">IPEZ dans le Game </a> 
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse"></button>
 
+             <div class="collapse navbar-collapse navHeaderCollapse">
+               <ul class="nav navbar-nav navbar-right">
+                 <li class="active"><a href="#">Home</a></li>
+                 <li><a href="#">Blog</a></li>
+                 <li class="dropdown">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Social Media</a>
+                   <ul class="dropdown-menu">
+                     <li><a href="#">Twitter</a></li>
+                     <li><a href="#">Facebook</a></li>
+                     <li><a href="#">Google+</a></li>
+                     <li><a href="#">Instagram</a></li>
+                   </ul>
+                 </li>
+                 <li><a href="#">About</a></li>
+                 <li><a href="#">Contact</a></li>
+               </ul>
+             </div>
+         </div>-->
+
+ 
+        <!-- End Header --> 
 
         <!-- Carousel 
         <ul class="bxslider">
@@ -25,15 +53,15 @@
             <p><span id="slider-prev"></span> | <span id="slider-next"></span></p>
         </div>
         <!-- /.carousel -->
-
+        <div class="container"></div>
 
 
         <!-- FOOTER -->
         <footer>
         </footer>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <script type="text/javascript" src="content/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="content/js/jquery.bxslider.min.js"></script>
+        <script type="text/javascript" src="app/content/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="app/content/js/jquery.bxslider.min.js"></script>
         <script type="text/javascript">
         $(document).ready(function(){
             $('.bxslider').bxSlider();
