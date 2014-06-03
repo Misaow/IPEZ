caae01f256d8abbbc6fb50ad5ff4fdcde4b7ed49
Misaow <yoann.pierre93@gmail.com>
2014-06-03 12:02:18 +0200

    Views Front

---
+++ Front/app/core/defines.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\core;
+
+$webroot= explode("/", $_SERVER['SCRIPT_NAME']);
+
+define( '__APP', true );
+define( 'DS', DIRECTORY_SEPARATOR );
+define( 'ADMIN_PATH', ROOT . DS . 'admin' );
+define( 'APP', ROOT . DS . 'app' );
+define( 'WEBROOT', '/' . $webroot[1]);
---
+++ Front/app/views/header.php
@@ -0,0 +1,5 @@
+<?php
+define( 'ROOT', dirname( __DIR__ ) );
+include ROOT.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'defines.php';
+include APP . DS . 'Autoloader.php';
+
