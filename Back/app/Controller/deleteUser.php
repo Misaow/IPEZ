1aa5823e75b6af19c092965640e613f377b9c746
ayile <ayile.delageot@gmail.com>
2014-06-03 17:01:48 +0200

    Controller User delete , create

--- Back/app/Controller/deleteUser.php
+++ Back/app/Controller/deleteUser.php
@@ -2,10 +2,10 @@
 
 $id = isset($_POST["id"]) ? $_POST["id"] : "";
 
-if(!empty($id)){
+ $userdelete = false;
+if(!empty($id) && (\app\core\User::isAdmin($_SESSION['id'], $_SESSION['login']) )){
   
    $user = new \app\core\User();
    $user->deleteUser($id);
-   
-    
+   $userdelete = true;
 }
