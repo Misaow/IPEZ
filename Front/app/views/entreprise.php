ac3ded9c7231b4e16d82c4f6985aa8f56983953e
Misaow <yoann.pierre93@gmail.com>
2014-06-06 12:11:43 +0200

    entreprise front

---
+++ Front/app/views/entreprise.php
@@ -0,0 +1,23 @@
+<?php
+include 'header.php';
+//include CONTROLLER_DIRECTORY . '/inscription.php';
+//fopen get le contenu 
+?>
+
+<div class="container maincontent">
+    <div class="row">
+        <div class="col-md-12 smallvid">
+            <div class="smallvid-title">
+                <h3>Inscription</h3>
+            </div>
+            <div class="smallvid-player" style="height: auto;">
+
+                <?php echo $textarea ?> 
+                 
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php';
+?>
+
